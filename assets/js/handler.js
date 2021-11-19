668e713e421250805904f2968e3c0f064a4318dd
Ophelie22 <59674073+Ophelie22@users.noreply.github.com>
2021-11-19 09:40:25 +0100

    on passe la tache en mode edition,quand le titre recoit le click

--- assets/js/handler.js
+++ assets/js/handler.js
@@ -9,13 +9,13 @@ let handler = {
     handleClickOnTaskTitle: function(event) {
         // traitement
          console.log('handleClickOnTaskTitle', event.currentTarget);
-        //let element = event.currentTarget;
+        let element = event.currentTarget;
 
         // récupérer le parent le plus proche qui a la classe task
         // https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
-        //let taskToModify = element.closest('.task');
+        let taskToModify = element.closest('.task');
 
         // ajouter la classe "task--edit"
-        //taskToModify.classList.add('task--edit');
+        taskToModify.classList.add('task--edit');
     },
 };
