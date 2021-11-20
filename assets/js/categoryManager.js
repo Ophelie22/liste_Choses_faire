004c8c7339d9da8fd5d7fbe44408d8b22f2f6bda
Ophelie22 <59674073+Ophelie22@users.noreply.github.com>
2021-11-20 01:36:22 +0100

    on cahr les categories d'en haut via du premier selecteur depuis l'API

--- assets/js/categoryManager.js
+++ assets/js/categoryManager.js
@@ -7,5 +7,47 @@ let categoryManager = {
      */
     loadCategories: function() {
         console.log('load categories');
-    }
+
+// On prépare la configuration de la requête HTTP
+        let config = {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache'
+      };
+        // On déclenche la requête HTTP (via le moteur sous-jacent Ajax)
+    fetch('https://benoclock.github.io/S07-todolist/categories.json', config)
+            // Ensuite, lorsqu'on reçoit la réponse au format JSON
+        .then(function(response) {
+                // console.log(response); // réponse entière (headers + contenu)
+                // On convertit cette réponse en un objet JS et on le retourne
+        return response.json();
+        })
+        // Ce résultat au format JS est récupéré en argument ici-même
+        .then(function(data) {
+        // On dispose désormais d'un tableau JS exploitable dans la variable data
+        
+          console.log(data); // contenu de la réponse sous forme d'objet JS
+
+          categoryManager.addOptionsToSelect(data);
+            }
+        ); 
+    },
+    /**
+     * Add options to a select item
+     */
+    //(categieArray je met le nom que je veux, j'ai besoin d'un tableau par rapport a data)
+    addOptionsToSelect: function(categoriesArray) {
+        // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Instructions/for...of
+        for (const currentCategory of categoriesArray) {
+            // console.log(currentCategory);
+
+            // create an option
+            let optionElement = document.createElement('option');
+            optionElement.textContent = currentCategory.name;
+            optionElement.value = currentCategory.id;
+
+            // add option to the select
+            document.getElementById('header-select-category').appendChild(optionElement);
+        }
+    },
 } 
